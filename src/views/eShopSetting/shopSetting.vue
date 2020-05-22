6cc7ffbd2c35e48b8c08d72985c35b622c21ab1c
Sellakannu <sellakannu@constient.com>
2020-05-22 22:06:34 +0530

    shop settings

--- src/views/eShopSetting/shopSetting.vue
+++ src/views/eShopSetting/shopSetting.vue
@@ -1,71 +1,100 @@
 <template>
   <el-row :gutter="0" type="flex" justify="center" class="centerSection">
-      <div class="sectionContainer">
-        <el-card class="box-card">
-          <el-form ref="shopData" :model="shopData">
-            <el-form-item label="E-Shop Name">
-              <el-input v-model="shopData.shop_name"></el-input>
-            </el-form-item>
-            <el-form-item label="E-Shop logo">
-              <el-upload
-                class="upload-demo"
-                action="https://jsonplaceholder.typicode.com/posts/"
-                :on-preview="handlePreview"
-                :on-remove="handleRemove"
-                :before-remove="beforeRemove"
-                multiple
-                :limit="1"
-                :file-list="fileList"
-              >
-                <el-button size="large">
-                  <i class="el-icon-upload2"></i>
-                </el-button>
-              </el-upload>
-            </el-form-item>
-            <el-form-item label="Languages">
-              <el-select v-model="language" multiple multiple-limit="3" placeholder="Select">
-                <el-option
-                  v-for="item in lang"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value"
-                ></el-option>
-              </el-select>
-            </el-form-item>
-            <el-form-item label="Region">
-              <el-select v-model="region" placeholder="Select">
-                <el-option
-                  v-for="item in regions"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value"
-                ></el-option>
-              </el-select>
-            </el-form-item>
-            <el-form-item label="Members">
-              <el-input v-model="shopData.members"></el-input>
-            </el-form-item>
-            <el-form-item label="Billing Currency">
-              <el-select
-                v-model="billing_currency"
-                multiple
-                multiple-limit="3"
-                placeholder="Select"
-              >
-                <el-option
-                  v-for="item in currency"
-                  :key="item.currency_id"
-                  :label="item.currency_name"
-                  :value="item.currency_id"
-                ></el-option>
-              </el-select>
-            </el-form-item>
-            <div class="text-right pt-10">
-                <el-button type="primary" size="small">Continue</el-button>
-            </div>
-          </el-form>
-        </el-card>
+    <div class="sectionContainer">
+      <el-card class="box-card">
+        <el-form ref="shopData" :model="shopData">
+          <el-form-item label="E-Shop Name">
+            <el-input v-model="shopData.shop_name"></el-input>
+          </el-form-item>
+          <el-form-item label="E-Shop logo">
+            <el-upload
+              class="upload-demo"
+              action="https://jsonplaceholder.typicode.com/posts/"
+              :on-preview="handlePreview"
+              :on-remove="handleRemove"
+              :before-remove="beforeRemove"
+              multiple
+              :limit="1"
+              :file-list="fileList"
+            >
+              <el-button size="large">
+                <i class="el-icon-upload2"></i>
+              </el-button>
+            </el-upload>
+          </el-form-item>
+          <el-form-item label="Languages">
+            <el-select v-model="language" multiple multiple-limit="3" placeholder="Select">
+              <el-option
+                v-for="item in lang"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="Region">
+            <el-select v-model="region" placeholder="Select">
+              <el-option
+                v-for="item in regions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="Members">
+            <el-input v-model="shopData.members"></el-input>
+            <el-button type="primary" size="small" @click="dialogTableVisible = true" class="memberAddButton">+</el-button>
+          </el-form-item>
+          <el-form-item label="Billing Currency">
+            <el-select v-model="billing_currency" multiple multiple-limit="3" placeholder="Select">
+              <el-option
+                v-for="item in currency"
+                :key="item.currency_id"
+                :label="item.currency_name"
+                :value="item.currency_id"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+          <div class="text-right pt-10">
+            <el-button type="primary" size="small">Continue</el-button>
+          </div>
+        </el-form>
+      </el-card>
+    </div>
+
+    <el-dialog title="Choose Role" :visible.sync="dialogTableVisible">
+      <div class="chooseRole">
+        <el-form ref="chooseRole" :model="chooseRole">
+          <el-form-item label="Email / Wechat ID">
+            <el-input v-model="chooseRole.email_wechatID"></el-input>
+          </el-form-item>
+          <!-- <h4 class="mb-0">Choose Role</h4> -->
+          <p>Whatever role you choose to assign, only you can transfer, duplicate or delete this site, or access the billing info.</p>
+          <el-radio-group v-model="radio">
+            <el-radio
+              :label="1"
+            >Admin: Has full access to the site but cannot edit the payment info, delete or duplicate the site.</el-radio>
+            <el-radio
+              :label="2"
+            >Back Office Manager: Can access the dashboard to manage site settings and apps but cannot edit the site.</el-radio>
+            <el-radio
+              :label="3"
+            >Website Manager: Can edit the site, manage settings and apps but cannor access inbox, contacts and other sensitive info.</el-radio>
+            <el-radio
+              :label="4"
+            >Bookings Admin: Has full access to your bookings calendar and contacts page, but cannot edit other areas of your site.</el-radio>
+            <el-radio
+              :label="5"
+            >Booking Staff Member: Can book their own clients, access their personal calendar and manage their sessions.</el-radio>
+          </el-radio-group>
+          <div class="text-right pt-10">
+            <el-button type="primary" size="small">Invite</el-button>
+            <el-button type="primary" size="small">Cancel</el-button>
+          </div>
+        </el-form>
       </div>
+    </el-dialog>
   </el-row>
 </template>
 
@@ -78,6 +107,12 @@ export default {
         shop_name: null,
         members: []
       },
+      // For Role Dialog
+      dialogTableVisible: false,
+      chooseRole: {
+        email_wechatID: null
+      },
+
       fileList: "",
       language: [],
       region: [],
@@ -162,11 +197,14 @@ export default {
   justify-content: center;
   background-color: #fff;
 }
+.mb-0 {
+    margin-bottom: 0;
+}
 .pt-10 {
-    padding-top: 10px;
+  padding-top: 10px;
 }
 .sectionContainer {
-    width: 450px;
+  width: 450px;
 }
 .centerSection {
   margin: 35px;
@@ -180,10 +218,28 @@ export default {
     }
   }
   .el-select {
-      display: block;
+    display: block;
   }
 }
 .text-right {
-    text-align: right;
+  text-align: right;
+}
+.chooseRole{
+    p {
+        font-size: 12px;
+    }
+}
+.el-radio {
+    margin-bottom: 10px;
+    display: flex!important;
+    .el-radio__label {
+        font-size: 12px;
+        white-space: normal;
+    }
+}
+.memberAddButton {
+    position: absolute;
+    right: 2px;
+    top: 2px;
 }
 </style>
--- src/views/eShopSetting/storeSetting.vue
+++ src/views/eShopSetting/storeSetting.vue
@@ -1,13 +1,140 @@
 <template>
-    <h3>Welcome to Store Setting</h3>
+  <el-row :gutter="0" type="flex" justify="center" class="centerSection">
+    <div class="sectionContainer">
+      <el-switch v-model="value1" active-text="" inactive-text="Store Status"></el-switch>
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="Diplay" name="display">
+          <el-form ref="display" :model="display">
+            <el-checkbox :label="1" v-model="checked">Quantity Box</el-checkbox>
+            <el-checkbox :label="2">Allow Product Reviews</el-checkbox>
+            <el-checkbox :label="3" v-model="checked">Auto Approve Reviews</el-checkbox>
+            <el-checkbox :label="4">Enable Wishlist</el-checkbox>
+            <el-checkbox :label="5">Enable Product Thumbnail Images</el-checkbox>
+            <el-checkbox :label="6" v-model="checked">Show Product Price</el-checkbox>
+            <el-checkbox :label="7">Show Product Brand</el-checkbox>
+            <el-checkbox :label="8" v-model="checked">Show Product Shopping Cost</el-checkbox>
+            <el-checkbox :label="9" v-model="checked">Show Product Rating</el-checkbox>
+            <el-checkbox :label="10" v-model="checked">Show Expected Delivery Date</el-checkbox>
+            <el-checkbox :label="11" v-model="checked">Show "Add to Cart" Option</el-checkbox>
+            <el-checkbox :label="12" v-model="checked">Show Product SKU</el-checkbox>
+            <el-form-item label="New Product Parameter Set Up">
+              <el-input v-model="display.enterNoOfDay" placeholder="Enter the no. of day"></el-input>
+            </el-form-item>
+
+            <p>Best Seller Parameter Set Up</p>
+            <el-checkbox :label="13" v-model="checked">Sales Volume</el-checkbox>
+            <el-checkbox :label="14">Product Rating</el-checkbox>
+            <el-checkbox :label="15">Sales Amount</el-checkbox>
+            <el-checkbox :label="16">Top Searched Products</el-checkbox>
+            <div class="text-right pt-10">
+              <el-button type="primary" size="small">Continue</el-button>
+            </div>
+          </el-form>
+        </el-tab-pane>
+        <el-tab-pane label="Security &amp; Privacy " name="securityPrivacy">
+          <p>Complex Password</p>
+          <el-checkbox
+            :label="securityPrivacy1"
+            v-model="checked"
+          >Enforce cpmplexity rule for shoppers password.</el-checkbox>
+          <p>Cookie Consent Tracking</p>
+          <el-checkbox :label="securityPrivacy2">Yes, turn on cookie consent banner in my site.</el-checkbox>
+          <p>Analutics for my business</p>
+          <el-checkbox :label="securityPrivacy3" v-model="checked">
+            Track customer on screen behaviour (
+            <a href="https://clicky.com/">https://clicky.com/</a>)
+          </el-checkbox>
+        </el-tab-pane>
+        <el-tab-pane label="Notification Settings" name="notificationSettings">
+          <p>Product Review Notification</p>
+          <el-checkbox
+            :label="notificationSettings1"
+          >Send message / emails to customers asking to review the purchased products.</el-checkbox>
+          <p>Forward Order Invoice</p>
+          <el-checkbox
+            :label="notificationSettings2"
+            v-model="checked"
+          >Send message / emails of their order invoices</el-checkbox>
+          <p>Forward Shipping Status</p>
+          <el-checkbox
+            :label="notificationSettings3"
+          >Send message / email whenever there is a change in the shipping status of the product.</el-checkbox>
+          <p class="checkboxHeading">
+            <el-checkbox :label="notificationSettings4" v-model="checked"></el-checkbox>Abandoned Card Notification
+          </p>
+          <p>Product Page Inventory Level Notification</p>
+          <el-checkbox
+            :label="notificationSettings5"
+          >Always notify the current inventory level of the product in the product page of the e-shop</el-checkbox>
+          <el-checkbox
+            :label="notificationSettings6"
+          >Notify the current inventory level if the product in the product page if the e-shop only if it falls below the low inventory level.</el-checkbox>
+          <el-checkbox
+            :label="notificationSettings7"
+            v-model="checked"
+          >Notify that the inventory level of this product is low but not the exact inventory level of the product in the product page of the e-shop, if it falls below the low inventory level.</el-checkbox>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+  </el-row>
 </template>
 
 <script>
-    export default {
-        name: "storeSetting"
-    }
+export default {
+  name: "storeSetting",
+  data() {
+    return {
+      checked: true,
+      value1: false,
+      activeName: "display",
+      display: {
+        enterNoOfDay: null
+      }
+    };
+  }
+};
 </script>
 
-<style scoped>
+<style lang="scss">
+.pt-10 {
+  padding-top: 10px;
+}
+.sectionContainer {
+  width: 550px;
+  background: #fff;
+  position: relative;
+  .el-tabs__content {
+    padding: 0 15px 15px;
+  }
+}
+.el-switch {
+    position: absolute!important;
+    right: 10px;
+    top: 10px;
+    z-index: 2;
+    .el-switch__label--left span {
+        font-size: 12px;
+    }
+}
+.el-tabs--top .el-tabs__item.is-top:nth-child(2) {
+  padding-left: 15px !important;
+}
+.text-right {
+  text-align: right;
+}
 
+.el-checkbox {
+  margin-bottom: 10px;
+  display: flex !important;
+  .el-checkbox__label {
+    font-size: 12px;
+    white-space: normal;
+  }
+}
+.checkboxHeading {
+  display: flex;
+  .el-checkbox:last-of-type {
+    margin-right: 10px;
+  }
+}
 </style>
